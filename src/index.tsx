ad101afd168ea0a0b7b05003dc38a39560609921
scub111 <scub111@mail.ru>
2019-03-12 23:10:09 +0300

    Work stage.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-/* render(
-    <App message="World" />,
-    document.getElementById('root'),
-); */
-
 render(
-    <h1>Hello</h1>,
+    <App message="World" />,
     document.getElementById('root'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
 const path = require('path');
 module.exports = {
-   entry: "./src/index.tsx",
-   output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, 'dist'),
-   },
-   devtool: 'sourse-map',
-   resolve: {
-      extensions: ['.js', '.json', '.ts', '.tsx']
-   },
-   module: {
-      rules: [
-         {
-            test: /\.ts$/,
-            // include: path.resolve(__dirname, 'src'),
-            loader: "awesome-typescript-loader"
-         }
-      ]
-   },
+    entry: './src/index.tsx',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.json', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'awesome-typescript-loader',
+            },
+        ],
+    },
 };
